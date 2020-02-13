ec41c8dedc196f2e4c3b5b3ddb6eb5bc4ca8be0e
Jeremy <Jeremy@leegame.net>
2020-02-13 20:19:25 +0800

    确定需求  替换了首页

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # v
 
 ```
-I need do sth to keep my finger dry. 
+stock              国内外    A股  美股  
+Bulk Stock   	   大宗商品  
+Futures contract   期货合约
+Virtual currency   虚拟货币
+Foreign exchange   外汇
 
-how about trade help application?
+大背景 + 做单记录 (自己加)   
+实时资讯 + 推送    
+技术分析(不要太杂) 
+复盘和预测        (自己加)
 
-keep memory of trade
 
-good, let's go.
+不需要密码  只需要账号即可
+只能新增  不能删除或者修改 
 ```
+
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,17 @@
   <div class="home">
 
 
-    <!-- TradingView Widget BEGIN -->
-    <div class="tradingview-widget-container">
-      <div id="tradingview_85d7b"></div>
-    </div>
-    <!-- TradingView Widget END -->
+<!--    &lt;!&ndash; TradingView Widget BEGIN &ndash;&gt;-->
+<!--    <div class="tradingview-widget-container">-->
+<!--      <div id="tradingview_85d7b"></div>-->
+<!--    </div>-->
+<!--    &lt;!&ndash; TradingView Widget END &ndash;&gt;-->
 
-    <!-- TradingView Widget BEGIN -->
-    <div class="tradingview-widget-container">
-      <div id="tradingview_cff2a"></div>
-    </div>
-    <!-- TradingView Widget END -->
+<!--    &lt;!&ndash; TradingView Widget BEGIN &ndash;&gt;-->
+<!--    <div class="tradingview-widget-container">-->
+<!--      <div id="tradingview_cff2a"></div>-->
+<!--    </div>-->
+<!--    &lt;!&ndash; TradingView Widget END &ndash;&gt;-->
 
 
   </div>
