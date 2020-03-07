c775729b0909156f93bd1bc04d50dc1871958c7f
Jeremy <Jeremy@leegame.net>
2020-03-07 16:59:15 +0800

    构建需求中

---
+++ src/views/Predict.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Predict">
+    <h1>This is an personal Predict page</h1>
+    <button @click="clickMe">click me jump to default page. baby</button>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'Predict',
+    methods: {
+      clickMe() {
+        this.$router.replace('/');
+      },
+    },
+  };
+</script>
